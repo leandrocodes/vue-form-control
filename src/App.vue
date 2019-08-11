fc6d1024d582a1cc93f545a98c694b6de041e2a3
Leandro <leaxviana140@gmail.com>
2019-08-10 22:09:17 -0400

    form finished

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,24 @@
 	<div id="app">
 		<h1>Formulário Desafio</h1>
 		<div class="conteudo">
-			<form v-if="!enviado" class="painel">
+			<form  class="painel">
 				<div class="cabecalho">Formulário</div>
+
+				<NomeCompleto v-model="nomecompleto"></NomeCompleto>
+				<rotulo nome="Email">
+					<input type="text" v-model="email">
+				</rotulo>
+
+				<rotulo nome="Senha">
+					<input type="password" v-model="senha">
+				</rotulo>
+
+				<rotulo nome="Armazenar?">
+					<input type="checkbox" v-model="dados">
+				</rotulo>
+				
+
+
 				<!-- Exercicio 01 -->
 				<!-- Criar uma formulário de registro -->
 				<!-- Nome completo (Nome e Sobrenome) -->
@@ -15,12 +31,24 @@
 				<!-- Só mostrar o fomulário de não tiver sido submetido -->
 				<!-- Mostrar a área de Resultado apenas quando o formulário for submetido -->
 				
-				<!-- Exercicio 03 -->
-				<!-- Crie um componente personalizado NomeCompleto -->
-				<!-- Esse componente deve receber Nome e Sobrenome -->
 			</form>
-			<div v-else class="painel">
+			<div  class="painel">
 				<div class="cabecalho">Resultado</div>
+				<Rotulo nome="Nome">
+					<span> {{nomecompleto.nome}} </span>
+				</Rotulo>
+				<Rotulo nome="Sobrenome">
+					<span> {{nomecompleto.sobrenome}} </span>
+				</Rotulo>
+				<Rotulo nome="Email">
+					<span> {{email}} </span>
+				</Rotulo>
+				<Rotulo nome="Senha">
+					<span> {{senha}} </span>
+				</Rotulo>
+				<Rotulo nome="Armazenar">
+					<span> {{dados}} </span>
+				</Rotulo>
 			</div>
 		</div>
 	</div>
@@ -32,10 +60,17 @@ import NomeCompleto from './components/NomeCompleto'
 
 export default {
 	name: 'app',
-	components: { Rotulo },
+	components: { Rotulo, NomeCompleto },
 	data(){
 		return{
 			enviado: false,
+			nomecompleto:{
+				nome: '',
+				sobrenome: ''
+			},
+			email:'',
+			senha:'',
+			dados: false
 		}
 	}
 }
--- src/components/NomeCompleto.vue
+++ src/components/NomeCompleto.vue
@@ -2,30 +2,42 @@
     <div class="nome-completo">
     
         <rotulo nome='Nome'>
-            <input type="text" v-model="usuario.nome">
+            <input type="text"  :value="value.nome" @input="alterarNome">
         </rotulo>
 
-        <rotulo nome='Senha'> 
-            <input type="password" v-model="usuario.senha">
+        <rotulo nome='Sobrenome'> 
+            <input type="text" :value="value.sobrenome" @input="alterarSobreNome">
         </rotulo>
-        
+
     </div>
 </template>
 
 <script>
 import rotulo from './Rotulo'
 export default {
+    props:{
+        value:{
+            type: Object,
+            required: true
+        }
+    },
+    methods:{
+        alterarNome($event){
+            this.$emit('input', {
+                nome: $event.target.value,
+                sobrenome: this.value.sobrenome
+            })
+        },
+        alterarSobreNome($event){
+            this.$emit('input', {
+                nome: this.value.nome,
+                sobrenome: $event.target.value
+            })
+        }
+    },
     components:{
         rotulo
     },
-    data(){
-        return{
-            usuario:{
-                nome: '',
-                sobrenome: ''
-            }
-        }
-    }
 }
 </script>
 
